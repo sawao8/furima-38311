612adb9056e634859459cb05250040106872998a
sawao8 <gzeben3@gmail.com>
2022-11-11 13:32:47 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence:true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do       
+    validates :last_name
+    validates :first_name 
+  end  
+  with_options presence:true, format: { with:/\A[ァ-ヶー]+\z/} do       
+    validates :last_name_kana
+    validates :first_name_kana 
+  end  
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
 end
