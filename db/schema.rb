2c8615a39fdc6d4654de0c81f7c2b09d130aa114
sawao8 <gzeben3@gmail.com>
2022-11-23 17:49:01 +0900

    商品出品情報を保存するためのモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_02_090242) do
+ActiveRecord::Schema.define(version: 2022_11_23_083224) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "item_content", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "price", null: false
+    t.integer "cost_id", null: false
+    t.integer "sender_area_id", null: false
+    t.integer "delivery_time_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_11_02_090242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
