b6004928e85ba49e35a5ce1e804439f53c08af03
sawao8 <gzeben3@gmail.com>
2022-11-24 14:59:45 +0900

    activehashのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,6 @@ class User < ApplicationRecord
   validates :birthday, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  has_many :items
 end
