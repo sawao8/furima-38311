c7cb928b3329eca0e477be1e651402924d6c148e
sawao8 <gzeben3@gmail.com>
2022-11-24 14:49:07 +0900

    activestorageとactivehashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :sender_area
+  belongs_to :delivery_time
 end
