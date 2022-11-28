afddde60dee8e4b7b959825cceb815bf121e7e47
sawao8 <gzeben3@gmail.com>
2022-11-29 02:26:45 +0900

    updateアクションのリーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -24,9 +24,10 @@ class ItemsController < ApplicationController
   def edit
     
   end
-  
 
-
+  def update
+    @item.update(item_params)
+  end  
     
   private
 
