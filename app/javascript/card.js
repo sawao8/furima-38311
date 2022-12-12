14bb80e67f68dcb0520d976a2855754f931afd03
sawao8 <gzeben3@gmail.com>
2022-12-13 04:54:46 +0900

    環境変数の修正(LGTM後実装?)

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("process.evn.PAYJP_PUBLIC_KEY");
+  Payjp.setPublicKey("pk_test_6ead44cb71b47c5adcd4aff6");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -9,23 +9,23 @@ const pay = () => {
 
     const card = {
       number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
       exp_month: formData.get("order_address[exp_month]"),
       exp_year: `20${formData.get("order_address[exp_year]")}`,
-      cvc: formData.get("order_address[cvc]"),
     };
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
-      document.getElementById("card_number").removeAttribute("name");
-      document.getElementById("card_exp_month").removeAttribute("name");
-      document.getElementById("card_exp_year").removeAttribute("name");
-      document.getElementById("card_cvc").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
