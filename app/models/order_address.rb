f63798f7f1d6618375bf6ea5181d3ab863c6f3a4
sawao8 <gzeben3@gmail.com>
2022-12-14 01:23:24 +0900

    電話番号バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :sender_area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
     validates :token
   end
 
