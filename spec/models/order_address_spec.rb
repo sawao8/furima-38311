945488976af5400f9b0f04a25584c1e4cd27910d
sawao8 <gzeben3@gmail.com>
2022-12-14 03:31:35 +0900

    テストコードの修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe OrderAddress, type: :model do
-  before do
-    @order_address = FactoryBot.build(:order_address)
-  end
+RSpec.describe OrderAddress, type: :model do 
 
   describe '配送先情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+      sleep(0.5)
+    end
+
+  
     context '配送先情報の保存ができるとき' do
       it 'すべての値が入力されていれば保存できる' do
         expect(@order_address).to be_valid
@@ -30,7 +35,7 @@ RSpec.describe OrderAddress, type: :model do
       it '郵便番号が空だと保存できない' do
         @order_address.post_code = nil
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Post code can't be blank", 'Post code is invalid')
+        expect(@order_address.errors.full_messages).to include("Post code can't be blank")
       end
       it '郵便番号にハイフンがないと保存できない' do
         @order_address.post_code = 1_234_567
@@ -64,6 +69,11 @@ RSpec.describe OrderAddress, type: :model do
       end
       it '電話番号にハイフンがあると保存できない' do
         @order_address.phone_number = '123-4567-8910'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が9桁以下だと保存できない' do
+        @order_address.phone_number = '123456789'
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number is invalid')
       end
